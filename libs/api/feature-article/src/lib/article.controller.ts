f72f047592572b6d3b7986ce77fa7638ac67adc1
DESKTOP-D8A7515\iampu <iampuneet.in@gmail.com>
2022-05-01 01:22:48 +0530

    feat: swagger configuration

--- libs/api/feature-article/src/lib/article.controller.ts
+++ libs/api/feature-article/src/lib/article.controller.ts
@@ -1,7 +1,9 @@
 import { ArticleService } from "@nest-app/api/data-access-article";
 import { Controller, Get } from "@nestjs/common";
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('articles')
+@ApiTags('articles')
 export class ArticleController {
     constructor( private articleService: ArticleService ) {
 
