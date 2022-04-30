26c959c055e4f509978b3a09710e5882c873d62c
DESKTOP-D8A7515\iampu <iampuneet.in@gmail.com>
2022-05-01 00:27:56 +0530

    feat: adding mongoose configuration

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
 import { FeatureConfigModule } from '@nest-app/api/feature-config';
+import { MongooseModule } from "@nestjs/mongoose"
+import { MongoConfiguration, mongoConfiguration } from '@nest-app-hour-nestjs/api/utils-config'
 @Module({
-  imports: [ FeatureConfigModule ],
+  imports: [ FeatureConfigModule,
+    // Put some dynamic stuff there
+    MongooseModule.forRootAsync({
+      inject: [
+        mongoConfiguration.KEY
+      ],
+      useFactory: (config: MongoConfiguration) => {
+        return {
+          uri: config.uri,
+          dbName: config.dbName
+        }
+      }
+    }) 
+  ],
   controllers: [],
   providers: [],
 })
--- libs/api/feature-config/src/feature-config.module.ts
+++ libs/api/feature-config/src/feature-config.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { appConfiguration } from '../../utils-config/src';
+import { appConfiguration, mongoConfiguration } from '../../utils-config/src';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       load: [
-        appConfiguration
+        appConfiguration,
+        mongoConfiguration
       ]
     })
   ],
